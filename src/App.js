cfaabdae7c48b3bff3756423d6dc71f5ed810efe
disha9112 <disha9112@gmail.com>
2021-10-11 16:14:01 +0530

    stored data in firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./components/shop/shop.component";
 import SignInAndSignUpPage from "./pages/homepage/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component.jsx";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +20,8 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
     });
   }
 
