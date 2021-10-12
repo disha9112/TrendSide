f40d3d82164f5dd56b1d2bdb3aabec4d56c5c922
disha9112 <disha9112@gmail.com>
2021-10-12 16:17:55 +0530

    initialized action

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
 import { Provider } from "react-redux";
+
+import store from "./redux/store";
 
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
