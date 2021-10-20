34e8458876d195719adbe9a6105f73c028569b06
disha9112 <disha9112@gmail.com>
2021-10-20 21:46:46 +0530

    initialized checkout page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./components/shop/shop.component";
 import SignInAndSignUpPage from "./pages/homepage/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/homepage/checkout/checkout.component";
 import Header from "./components/header/header.component.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -46,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             path="/signin"
             render={() =>
